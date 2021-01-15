25b7232612efe425a96b4284268b65d62be45d0b
Rafał Komorowski <komor@mac.com>
2021-01-15 21:48:48 +0100

    Fix Remote JSON data with JS example
    Remote JSON fetching JS code is triggered on every page (via layout.erb calling custom.js). Should be called on json_js.html.erb pages only.

--- source/statics/js/custom.js
+++ source/statics/js/custom.js
@@ -4,19 +4,22 @@ $(document).ready(function () {
     $("[data-toggle=popover]").popover({
         trigger: 'focus'
     })
-
-    // JSON data sample
-    fetch('https://newtablab.com/test/db.json')
-    .then(response => response.json())
-    .then(data => {
-        data.posts.map(item => {
-            $('#remote-posts').append(`<li>${item.id} - ${item.title}</li>`);
-        });
-        data.fruits.map(item => {
-            $('#remote-fruits').append(`<li>${item}</li>`);
-        });
-        data.people.map(item => {
-            $('#remote-people').append(`<li>${item}</li>`);
-        });
-    });
 });
+
+// JSON data sample, see /source/pages/json_js.*.html.erb
+function remote_json_fetch() {
+  console.log("JSON fetch.");
+  fetch('https://newtablab.com/test/db.json')
+  .then(response => response.json())
+  .then(data => {
+      data.posts.map(item => {
+          $('#remote-posts').append(`<li>${item.id} - ${item.title}</li>`);
+      });
+      data.fruits.map(item => {
+          $('#remote-fruits').append(`<li>${item}</li>`);
+      });
+      data.people.map(item => {
+          $('#remote-people').append(`<li>${item}</li>`);
+      });
+  });
+}
